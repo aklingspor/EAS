b62763e21262ea8187fd4fa228697ed85f45dd2f
Anton Klingspor <a@antonklingspor.com>
2021-02-11 10:23:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo ("Hello!");
+$queryString = http_build_query([
+  'access_key' => '08d9a9c3b45bf0be7f7627fbc557f953'
+]);
+
+$ch = curl_init(sprintf('%s?%s', 'https://api.marketstack.com/v1/tickers/aapl/eod', $queryString));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$json = curl_exec($ch);
+curl_close($ch);
+
+$apiResult = json_decode($json, true);
+
+foreach ($apiResult['data'] as $stocksData) {
+  echo sprintf('Ticker %s has a day high of %s on %s', $stockData['symbol'], $stockData['high'], $stockData['date'] )
+}
 ?>
