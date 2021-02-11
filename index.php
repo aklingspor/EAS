20c086d23bef15b7ca474205b1cc75b3978224b7
Anton Klingspor <a@antonklingspor.com>
2021-02-11 10:46:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<form action="getdata.php" method="post"> <input type="text" name="ticker"> <br> <input type="submit"></form> 
+<form action="getdata.php" method="post"> <input type="text" name="ticker" id="ticker"> <br> <input type="submit"></form> 
